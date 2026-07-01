90de8190c46c6d0f591f8a0af587ee7d676dd829
Krish Patel <krishnpatelop2@gmail.com>
2026-07-01 19:13:19 +0530

    Endpoints added

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from services.password_service import analyze_password
 
 app = FastAPI(
     title="Password Security Toolkit",
-    version="1.0.0"
+    version="1.0.0",
+    description="API to analyze password security."
 )
+
+class PasswordRequest(BaseModel):
+    password: str
 
 @app.get("/")
 def home():
     return {
-        "message": "Password Security Toolkit API is running!"
+        "message": "Welcome to Password Security Toolkit!"
     }
+
+@app.post("/analyze-password")
+def analyze(request: PasswordRequest):
+    return analyze_password(request.password)
