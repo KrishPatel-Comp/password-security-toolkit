70e17c6480c5a4f508f683f7a5ebc367521b5ef2
Krish Patel <krishnpatelop2@gmail.com>
2026-07-02 10:37:53 +0530

    Organized Entropy

--- Backend/services/password_service.py
+++ Backend/services/password_service.py
@@ -1,44 +1,6 @@
-import math
 from pathlib import Path
+from utils.entropy import calculate_entropy, estimate_crack_time
 
-def calculate_entropy(password: str):
-
-    charset = 0
-
-    if any(c.islower() for c in password):
-        charset += 26
-
-    if any(c.isupper() for c in password):
-        charset += 26
-
-    if any(c.isdigit() for c in password):
-        charset += 10
-
-    if any(not c.isalnum() for c in password):
-        charset += 32
-
-    if charset == 0:
-        return 0
-
-    entropy = len(password) * math.log2(charset)
-
-    return round(entropy, 2)
-def estimate_crack_time(entropy):
-
-    if entropy < 28:
-        return "Instantly"
-
-    elif entropy < 36:
-        return "A few minutes"
-
-    elif entropy < 60:
-        return "Several hours"
-
-    elif entropy < 80:
-        return "Several years"
-
-    else:
-        return "Centuries"   
     
 def detect_repeated_characters(password: str):
 
